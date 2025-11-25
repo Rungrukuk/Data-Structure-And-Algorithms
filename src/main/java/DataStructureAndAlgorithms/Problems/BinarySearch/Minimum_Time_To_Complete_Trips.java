54423d37ca0c1119cfc43515628dccb3b3f2c6fd
Rungrukuk <rungrukuk@gmail.com>
2025-11-25 16:30:12 +0400

    Add category to Minimum Time To Complete Trips problem

--- src/main/java/DataStructureAndAlgorithms/Problems/BinarySearch/Minimum_Time_To_Complete_Trips.java
+++ src/main/java/DataStructureAndAlgorithms/Problems/BinarySearch/Minimum_Time_To_Complete_Trips.java
@@ -3,7 +3,7 @@ package DataStructureAndAlgorithms.Problems.BinarySearch;
 import DataStructureAndAlgorithms.Base_Problem;
 import DataStructureAndAlgorithms.Problem;
 
-@Problem(name = "Minimum Time To Complete Trips")
+@Problem(name = "Minimum Time To Complete Trips", category = "Binary Search")
 public class Minimum_Time_To_Complete_Trips extends Base_Problem<Long> {
     public final int[] time = new int[] { 39855, 21385, 86948, 57163, 24158, 85790, 95497, 9931, 31399, 15301, 30502,
             9864, 90569, 76768, 76558, 25520, 53221, 54640, 8598, 93891, 4852, 32387, 44867, 47670, 23128, 75389, 1345,
