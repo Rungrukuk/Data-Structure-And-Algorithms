838525c9918ef034414b6f12fee0cce9c5be358b
Rungrukuk <rungrukuk@gmail.com>
2025-12-07 15:03:21 +0400

    Practiced the easy problems of the binary search category

---
+++ src/main/java/DataStructureAndAlgorithms/Practices/BinarySearch/BinarySearch_Practice.java
@@ -0,0 +1,31 @@
+package DataStructureAndAlgorithms.Practices.BinarySearch;
+
+import DataStructureAndAlgorithms.core.base.BasePractice;
+import DataStructureAndAlgorithms.core.annotations.Practice;
+import DataStructureAndAlgorithms.Problems.BinarySearch.BinarySearch;
+
+@Practice(problemName = "Binary Search", category = "Binary Search")
+public class BinarySearch_Practice extends BasePractice<Integer, BinarySearch> {
+
+    public BinarySearch_Practice(BinarySearch problem) {
+        super(problem);
+    }
+
+    @Override
+    public Integer practice() {
+        int left = 0;
+        int right = problem.nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (problem.nums[middle] == problem.target) {
+                return middle;
+            }
+            if (problem.nums[middle] > problem.target) {
+                right = middle - 1;
+            } else
+                left = middle + 1;
+        }
+
+        return -1;
+    }
+}
