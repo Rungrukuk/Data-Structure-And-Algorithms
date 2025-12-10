5e1cdc9690733a97c465064e661cd227173aa778
Rungrukuk <rungrukuk@gmail.com>
2025-12-10 15:25:28 +0400

    created the problem

---
+++ src/main/java/DataStructureAndAlgorithms/Problems/BinarySearch/KthSmallestPairDistance.java
@@ -0,0 +1,17 @@
+package DataStructureAndAlgorithms.Problems.BinarySearch;
+
+import DataStructureAndAlgorithms.core.base.BaseProblem;
+import DataStructureAndAlgorithms.core.annotations.Problem;
+
+@Problem(name = "Kth Smallest Pair Distance", category = "Binary Search")
+public class KthSmallestPairDistance extends BaseProblem<Integer> {
+
+    public int[] nums = new int[] { 1, 6, 1 };
+    public int k = 3;
+
+    @Override
+    public Integer solve() {
+        // TODO: Implement solution
+        throw new UnsupportedOperationException("Unimplemented method 'solve'");
+    }
+}
